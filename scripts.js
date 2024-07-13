13fb1a1bf7d5f83c0a55b45628260a134c1e2b66
Nahasminar <nahasniyaz@gmail.com>
2024-07-13 10:37:36 +0300

    Initial commit: added website files

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.getElementById('facebook-support').addEventListener('click', () => {
+    window.location.href = 'https://www.facebook.com/profile.php?id=100093851127885';
+});
+document.getElementById('99951466033').addEventListener('click', () => {
+    window.location.href = 'https://whatsapp.com';
+});
